5fb28580ec4346ac9b23e67db74176717aa0b1b9
tofan07 <tofan0797@gmail.com>
2021-04-14 20:37:19 +0300

    refact validate js

--- src/js/modules/validate.js
+++ src/js/modules/validate.js
@@ -1,28 +1,24 @@
-const nameValidate = input => {
-    if (input) {
-        const nameAttr = input.getAttribute('name')
-        switch (nameAttr) {
-            case 'name':
-                console.log("it is name input");
-                break;
-            case 'email':
-                console.log("it is email input");
-                break;
-            case 'location':
-                console.log("it is location input");
-                break;
-            case 'phone':
-                console.log("it is phone input");
-                break;
-        }
-    }
-
-}
 
 export const validate = (form) => {
     form.addEventListener('input', event => {
         let input = event.target;
 
-        nameValidate(input);
+        if (input) {
+            const nameAttr = input.getAttribute('name')
+            switch (nameAttr) {
+                case 'name':
+                    console.log("it is name input");
+                    break;
+                case 'email':
+                    console.log("it is email input");
+                    break;
+                case 'location':
+                    console.log("it is location input");
+                    break;
+                case 'phone':
+                    console.log("it is phone input");
+                    break;
+            }
+        }
     });
 }
